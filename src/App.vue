7f70d787f64962b0b3e41f4f15c1430c8caab699
augustoprogramador <augusto.camargo@aluno.ifsp.edu.br>
2024-05-15 20:20:06 -0300

    Propriedades computadas

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <div>
-    <button
-      v-on:click="onClick"
-      @mouseover="passei"
-    >Enviar</button>
+  <input v-model="user.firstName" type="text" @keyup="digitei">
+  <input v-model="user.lastName" type="text" @keyup="digitei">
+  <h1>{{ fullName }}</h1>
+
+  <h2>Todos Incompletas</h2>
+  <div 
+    v-for="todo in unfinishedTodos"
+    v-bind:key="todo.id"
+  >
+    {{ todo.title }}
   </div>
 
-  <form action="" @submit.prevent="submit">
-    <input type="text" @keyup="digitei">
-    <button type="submit">Enviar form</button>
-  </form>
+  <h2>Todos Completas</h2>
+  <div 
+    v-for="todo in finishedTodos"
+    v-bind:key="todo.id"
+  >
+    {{ todo.title }}
+  </div>
+
+  <h2>Todos</h2>
+  <div 
+    v-for="todo in todos"
+    v-bind:key="todo.id"
+  >
+    <input 
+      type="checkbox" 
+      :id="`todo-${todo.id}`"
+      :value="todo.completed"
+      v-model="todo.completed"
+      :disabled="todo.completed"
+    >
+    <label :for="`todo-${todo.id}`">{{ todo.title }}</label>
+  </div>
 </template>
 
 <script>
@@ -20,11 +43,58 @@ export default {
   },
   data() {
     return {
+      user: {
+        firstName: 'Augusto',
+        lastName: 'Camargo',
+      },
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+      ]
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    },
+    unfinishedTodos() {
+      return this.todos.filter(todo => !todo.completed);
+    },
+    finishedTodos() {
+      return this.todos.filter(todo => todo.completed);
     }
   },
   methods: {
     onClick() {
-      console.log('Cliquei');
+      console.log('Cliquei', this);
     },
     passei() {
       console.log('passei');
@@ -39,5 +109,4 @@ export default {
 }
 </script>
 
-<style>
-</style>
+<style></style>
