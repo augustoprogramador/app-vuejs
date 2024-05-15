4bd43bcc6a6e007b9d1659e8033930803d968ef1
augustoprogramador <augusto.camargo@aluno.ifsp.edu.br>
2024-05-15 14:07:39 -0300

    Aprendendo a iterar objetos gerando interfaces dinâmicas

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,70 @@
 <template>
-  <AppHeader
-    v-show="showHeader"
+  <div 
+    class="todo-item"
+    v-for="(item, index) in todos"
+    v-bind:key="item.id"
   >
-  </AppHeader>
-  <div>
-    Nome: {{ name }}
+    {{ index }} - {{ item.title }}
   </div>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import AppHeader from './components/AppHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld, 
-    AppHeader
+    HelloWorld,
   },
   data() {
     return {
-      showHeader: true,
-      name: 'Augusto Camargo'
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+      ]
     }
   }
 }
 </script>
 
 <style>
+.todo-item {
+  background-color: #000;
+  color: #fff;
+  margin: 0 0 5px;
+  padding: 3px 6px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
