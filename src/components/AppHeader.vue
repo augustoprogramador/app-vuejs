df84b7c0ccc0ce71e8e593ad03796edbb54e1a03
augustoprogramador <augusto.camargo@aluno.ifsp.edu.br>
2024-05-15 11:08:39 -0300

    criando o primeiro componente

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="header">Headerrrr</div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+    .header{
+        color: white;
+        background-color: black;
+    }
+</style>
