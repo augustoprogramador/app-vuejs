1d05dcde74011a9a984ffc2f1a2b9c13cd2d3322
augustoprogramador <augusto.camargo@aluno.ifsp.edu.br>
2024-05-16 09:37:23 -0300

    Trabalhando com Hooks e LifeCycle

--- src/App.vue
+++ src/App.vue
@@ -1,128 +1,69 @@
 <template>
   <div>
+    <AppHeader v-if="showHeader"/>
+    <h1>Hello World!</h1>
+    <button @click="showHeader = !showHeader">{{ showHeader ? 'Esconder' : 'Mostrar' }}</button>
     <input 
       type="text"
       v-model="name"
     >
-    <br/>
     {{ name }}
   </div>
-  <h1>User</h1>
-  <div>
-    <input 
-      type="text"
-      v-model="user.firstName"
-    >
-    <br/>
-    {{ user.firstName }}
-  </div>
-  <br/>
-  <label for="paginacao">Paginação</label>
-  <select v-model="pageCount" name="paginacao" id="paginacao">
-    <option value="5">5</option>
-    <option value="10">10</option>
-    <option value="15">15</option>
-  </select>
-  {{ pageCount }}
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue';
 
 export default {
   name: 'App',
   components: {
+    AppHeader
   },
   data() {
     return {
-      name: '',
-      pageCount: 0,
-      user: {
-        firstName: 'Augusto',
-        lastName: 'Camargo',
-      },
-      todos: [
-        {
-          "userId": 1,
-          "id": 1,
-          "title": "delectus aut autem",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 2,
-          "title": "quis ut nam facilis et officia qui",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 3,
-          "title": "fugiat veniam minus",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 4,
-          "title": "et porro tempora",
-          "completed": true
-        },
-        {
-          "userId": 1,
-          "id": 5,
-          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-          "completed": false
-        }
-      ]
+      name: 'Augusto',
+      showHeader: true
     }
   },
-  watch: { // Observer
-    // name(newValue, oldValue) {
-    //   console.log(newValue, oldValue);
-    // },
-    name(value) {
-      if (value.length >= 3)
-        this.saveUserName();
-    },
-    pageCount() {
-      this.changePageCount();
-    },
-    user: { // TRANSFORMEI EM OBJETO
-      handler() {
-        console.log(this.user);
-      },
-      deep: true
-    },
+
+  // HOOKS
+  // beforeCreate() {
+  //   console.log('beforeCreate', {
+  //     Estado: this.name,
+  //     DOM: this.$el
+  //   })
+  // },
+  // created() {
+  //   console.log('created', {
+  //     Estado: this.name,
+  //     DOM: this.$el
+  //   })
+  // },
+  // beforeMount() {
+  //   console.log('beforeMount', {
+  //     Estado: this.name,
+  //     DOM: this.$el
+  //   })
+  // },
+  // mounted() {
+  //   console.log('mounted', {
+  //     Estado: this.name,
+  //     DOM: this.$el
+  //   })
+  // },
+  beforeUpdate() {
+    console.log('beforeUpdate')
+  },
+  updated() {
+    console.log('updated')
+  },
+
+
+  watch: {
   },
   computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`
-    },
-    unfinishedTodos() {
-      return this.todos.filter(todo => !todo.completed);
-    },
-    finishedTodos() {
-      return this.todos.filter(todo => todo.completed);
-    }
   },
   methods: {
-    onClick() {
-      console.log('Cliquei', this);
-    },
-    passei() {
-      console.log('passei');
-    },
-    submit() {
-      console.log('enviei o form')
-    },
-    digitei(event) {
-      console.log('digitei no form', event.target.value)
-    },
-    saveUserName() {
-      console.log('Ajax');
-      console.log(this.name);
-    },
-    changePageCount() {
-      console.log('changePageCount');
-    }
   }
 }
 </script>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,34 @@
 <template>
-    <div class="header">Headerrrr</div>
+  <div class="header">Headerrrr</div>
 </template>
 
 <script>
-    export default {
+export default {
+  data() {
+    return {
+      
     }
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount', {
+      Estado: this.name,
+      DOM: this.$el
+    })
+  },
+  unmounted() {
+    console.log('unmounted', {
+      Estado: this.name,
+      DOM: this.$el
+    });
+  },
+  methods: {
+  }
+}
 </script>
 
 <style>
-    .header{
-        color: white;
-        background-color: black;
-    }
+.header {
+  color: white;
+  background-color: black;
+}
 </style>
