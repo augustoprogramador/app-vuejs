3d52e4e811483790699abd6deaed882a716742e9
augustoprogramador <augusto.camargo@aluno.ifsp.edu.br>
2024-05-17 09:50:21 -0300

    trabalhando com eventos no Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,26 @@
 <template>
-  <AppHook v-if="showAppHook"/>
-  <br/>
-  <br/>
-  <button @click="showAppHook = !showAppHook">Destruir Hook</button>
-  <br/>
-  <img @click="changeName()" alt="Vue logo" src="./assets/logo.png">
-  {{ name }}<br/><br/>
-  {{ user.firstName }}<br/><br/>
-  {{ admin.admin }}<br/><br/>
-  <br/>
-  Nome Completo: {{ fullName }} <button @click="user.firstName = 'Ludimila'">Atualizar nome</button>
+  <AppButton
+    data-vue="teste"
+    @update="getUpdate"
+  >
+    Save
+  </AppButton>
+  Hello Vue
 </template>
 
 <script>
-import { computed, reactive, ref, watch } from 'vue';
-import AppHook from './components/AppHook.vue';
+import AppButton from '@/components/AppButton.vue';
 
 export default {
   name: 'App',
   components: {
-    AppHook
+    AppButton,
   },
-  // Options API
-  /**
-   * computed: {
-   *    fullName() {
-   *      return '';
-   *    }
-   * }
-   */
-  // Alternativa ao option API que só pode criar atributos reativos
-  setup() { // composition API permite controlar o que desejamos que seja reativo
-    let name = 'Augusto';
-
-    const user = reactive({
-      firstName: 'Augusto',
-      lastName: 'Camargo'
-    });
-
-    const admin = ref({
-      admin: 'Admin',
-      login: 'MASTER'
-    });
-
-    const showAppHook = ref(true);
-
-    const changeName = () => {
-      name = 'Augusto Camargo';
-      user.firstName = 'Augusto S';
-      // Precisamos usar o .value quando usamos o red, pois ele encapsula o objeto
-      // No entanto, não necessitamos disso no template
-      admin.value.admin = 'XXXXXX';
-    }
-
-    const fullName = computed(() => {
-      return `${user.firstName} ${user.lastName}`
-    });
-
-    watch(admin, () => {
-      console.log('estou de olho');
-    });
-    // }, {deep: true}); // para observar profundamente, não necessário quando a variável for reactive()
-    // Outra opção seria retornar o atributo que queremos observar: () => admin.value.firstName
+  setup() {
+    const getUpdate = (data) => {console.log('updated', data)};
 
     return {
-      name,
-      user,
-      admin,
-      fullName,
-      showAppHook,
-      changeName,
+      getUpdate,
     }
   }
 }
@@ -80,7 +31,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/AppButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <button @click="sendData">
+        {{ variant }}
+    </button>
+</template>
+<script>
+    export default {
+        props: {
+            variant: {
+                type: String,
+                default: 'success'
+            }
+        },
+        /**
+         * setup() recebe 2 parâmetros: As Propriedades e o Contexto
+         * O contexto tem acesso ao atributos do componente, por exemplo, se na tag de chamada
+         * acrescentarmos um data-vue="alguma coisa", esse atributo e seu valor estarão disponíveis no
+         * setup do componente através do contexto, assim como muitas outras coisas
+         */
+        setup(props, {emit}) { // Passando as porps por parâmetro, já que não temos acesso ao this dentro do setup
+
+            const sendData = () => {
+                emit('update', true);
+            }
+            return {
+                sendData,
+            }
+        }
+    }
+</script>
